f9478378c1fd9e59a9b325c676e40834268c074f
wilsonhm21 <whuacasimamani@gmail.com>
2025-06-25 11:28:39 -0500

    aws

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_lambda_function" "csv_processor" {
   handler       = "app.lambda_handler"
   runtime       = "python3.10"
   role          = aws_iam_role.lambda_exec_role.arn
-  filename = var.lambda_zip_path
+  filename      = var.lambda_zip_path
   source_code_hash = filebase64sha256(var.lambda_zip_path)
 
 
